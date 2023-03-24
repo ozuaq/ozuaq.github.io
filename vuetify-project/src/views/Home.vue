843313416900c14c725a864dac752cfe15db72f9
ozuaq <oza4456@gmail.com>
2023-03-25 00:41:37 +0900

    add components/ButtonCounter & components/TwitterCard

--- vuetify-project/src/views/Home.vue
+++ vuetify-project/src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <HelloWorld />
+  <!--
+    <HelloWorld />
+  -->
+  <TwitterCard :msg="msg" />
+  <ButtonCounter class="my-4" />
+  <ButtonCounter />
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  //import HelloWorld from '@/components/HelloWorld.vue'
+  import TwitterCard from '@/components/TwitterCard.vue'
+  import ButtonCounter from '@/components/ButtonCounter.vue';
+
+  const msg = "Vueってやっぱおもろいわ〜!";
 </script>
